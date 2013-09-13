ef1fd9ba03badf26eb74ef7bec82285ad7955d1d
Kate von Roeder <katevonroeder@gmail.com>
2013-09-13 06:49:54 -0700

    Add in wins/losses

--- app/models/fight.rb
+++ app/models/fight.rb
@@ -3,23 +3,20 @@ class Fight < ActiveRecord::Base
   belongs_to :red_champion, class_name: 'Champion'
 
   def update_champions
-    unless self.champions_updated
-      red_champion.total_bets  += bets_red
-      blue_champion.total_bets += bets_blue
+    red_champion.total_bets  += bets_red
+    blue_champion.total_bets += bets_blue
 
-      if winner == 1
-        red_champion.wins    += 1
-        blue_champion.losses += 1
-      else
-        red_champion.losses += 1
-        blue_champion.wins  += 1
-      end
-
-      self.champions_updated=true
-      save
-      red_champion.save
-      blue_champion.save
+    if winner == 1
+      red_champion.wins    += 1
+      blue_champion.losses += 1
+    else
+      red_champion.losses += 1
+      blue_champion.wins  += 1
     end
 
+    red_champion.save
+    blue_champion.save
   end
+
 end
+
