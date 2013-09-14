43b14bac00d51999778dca2fa3c1f12696438fbc
Kate von Roeder <katevonroeder@gmail.com>
2013-09-14 03:14:57 -0700

    Forcibly downcase names

--- app/controllers/scraper_controller.rb
+++ app/controllers/scraper_controller.rb
@@ -58,12 +58,12 @@ class ScraperController < ApplicationController
       matchup      = element.children[0].children[0]
       bet_count    = element.children[8].text
       winner_color = element.children[2].children[0].attr('class')
-      red_name     = matchup.children[0].text.downcase
-      blue_name    = matchup.children[2].text.downcase
+      red_name     = matchup.children[0].text
+      blue_name    = matchup.children[2].text
       red_value    = matchup.children[1].text.gsub(/[^0-9]/, '')
       blue_value   = matchup.children[3].text.gsub(/[^0-9]/, '')
-      red_champ    = Champion.find_or_create_by(name: red_name)
-      blue_champ   = Champion.find_or_create_by(name: blue_name)
+      red_champ    = Champion.find_or_create_by(name: red_name.downcase)
+      blue_champ   = Champion.find_or_create_by(name: blue_name.downcase)
 
       fight_entry = Fight.new(bet_count: bet_count, bets_red: red_value, bets_blue: blue_value)
 
