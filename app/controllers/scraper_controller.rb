b78178f467fe472993890bf6d6ea2801f845b339
Kate von Roeder <katevonroeder@gmail.com>
2013-09-14 14:40:41 -0700

    Skip fights already in the DB

--- app/controllers/scraper_controller.rb
+++ app/controllers/scraper_controller.rb
@@ -41,12 +41,19 @@ class ScraperController < ApplicationController
   #Takes a Mechanize::Page
   def get_tournament_entries(tourneypage)
     logger.info('Scraping ' + tourneypage.uri.to_s)
+    skip_count = 0
     tourneypage.search('//table/tbody/tr').each do |entry|
-      parse_entry(entry)
+      unless parse_entry(entry)
+        skip_count += 1
+      end
     end
     links_with = tourneypage.links_with(:text => 'Next')
-    if links_with.length>0
-      get_tournament_entries(links_with[0].click)
+    if skip_count==0
+      if links_with.length>0
+        get_tournament_entries(links_with[0].click)
+      end
+    else
+      logger.info("Skipped #{skip_count} matches, we've already scraped!")
     end
 
   end
@@ -59,7 +66,9 @@ class ScraperController < ApplicationController
       match_id = matchup.attr('href').gsub(/[^0-9]+/, '')
 
       # First check if we have an extant match entry for this particular fight.
-      unless Fight.find_by_match_id(match_id)
+      if Fight.find_by_match_id(match_id)
+        return false
+      else
         bet_count    = element.children[8].text
         winner_color = element.children[2].children[0].attr('class')
         red_name     = matchup.children[0].text
@@ -68,7 +77,6 @@ class ScraperController < ApplicationController
         blue_value   = matchup.children[3].text.gsub(/[^0-9]+/, '')
         red_champ    = Champion.find_or_create_by(name: red_name.downcase)
         blue_champ   = Champion.find_or_create_by(name: blue_name.downcase)
-
 
         fight_entry               = Fight.new(bet_count: bet_count, bets_red: red_value, bets_blue: blue_value, match_id: match_id)
         fight_entry.blue_champion = blue_champ
@@ -82,6 +90,7 @@ class ScraperController < ApplicationController
 
         fight_entry.update_champions
         fight_entry.save
+        return true
       end
     rescue => error
       logger.warn(error)
